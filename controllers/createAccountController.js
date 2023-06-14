2bd48685730354f106914566b228963b148bccc6
Praneeth Ravuri <prav2500@gmail.com>
2023-06-15 00:39:28 +0530

    Validate User

--- controllers/createAccountController.js
+++ controllers/createAccountController.js
@@ -13,18 +13,24 @@ const userCreateAccount = asyncHandler(async (req, res) => {
     throw new Error("All fields are mandatory");
   }
 
+  const emailAvailable = await Credentials.findOne({ email });
+
+  if (emailAvailable) {
+    res.status(400);
+    throw new Error("This email address is already in use");
+  }
+
   const hashedPassword = await brcypt.hash(password, 10);
   console.log(`Hashed password : ${hashedPassword}`);
 
   const createUser = await Credentials.create({
     email,
-    password : hashedPassword
+    password: hashedPassword,
   });
 
-  if(createUser){
-    res.status(201).json({_id : createUser.id, email : createUser.email});
-  }
-  else{
+  if (createUser) {
+    res.status(201).json({ _id: createUser.id, email: createUser.email });
+  } else {
     res.status(400);
     throw new Error("User data is not valid");
   }
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,12 +1,28 @@
 const asyncHandler = require("express-async-handler");
-const Login = require("../models/credentialsModel");
+const Credentials = require("../models/credentialsModel");
+const bcrypt = require("bcrypt");
 
-//@desc get the login and password from the json body
+//@desc User login
 //@route POST /login
 //@access Public
 
 const userLogin = asyncHandler(async (req, res) => {
-    res.status(200).json(req.body);
+  const { email, password } = req.body;
+
+  const credentials = await Credentials.findOne({ email });
+
+  if (!credentials) {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, credentials.password);
+
+  if (isPasswordMatch) {
+    res.status(200).json({ message: `Login Successful ${credentials.password}` });
+  } else {
+    res.status(401).json({ message: "Invalid email or password" });
+  }
 });
 
-module.exports = {userLogin};
+module.exports = { userLogin };
